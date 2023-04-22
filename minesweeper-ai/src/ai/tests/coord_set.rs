26d03dcd5ed23b1b59deb7328b3802514671d53d
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2023-04-22 19:41:05 +0300

    Add tests for coord_set

--- minesweeper-ai/src/ai/tests/coord_set.rs
+++ minesweeper-ai/src/ai/tests/coord_set.rs
@@ -1,19 +1,214 @@
-use miinaharava::minefield::Matrix;
+use miinaharava::minefield::{Coord, Matrix};
 
 use crate::ai::coord_set::CoordSet;
 
 #[test]
-fn test_coord_set_is_empty() {
-    for _ in 0..1000 {
-        let mut set = CoordSet {
-            matrix: Matrix([[false; 30]; 30]),
-        };
-        set.matrix.0.fill_with(|| {
-            let mut array = [false; 30];
-            array.fill_with(rand::random);
-            array
-        });
-
+fn test_is_empty() {
+    for _ in 0..10000 {
+        let set = get_random_set::<30, 30>();
         assert!(set.is_empty() == (set.iter().count() == 0))
     }
+}
+
+#[test]
+fn test_corners() {
+    let expected = CoordSet {
+        matrix: Matrix([
+            [true, false, false, false, true],
+            [false; 5],
+            [false; 5],
+            [false; 5],
+            [true, false, false, false, true],
+        ]),
+    };
+
+    assert_eq!(CoordSet::corners(), expected);
+}
+
+#[test]
+fn test_edges() {
+    let expected = CoordSet {
+        matrix: Matrix([
+            [false, true, true, true, false],
+            [true, false, false, false, true],
+            [true, false, false, false, true],
+            [true, false, false, false, true],
+            [false, true, true, true, false],
+        ]),
+    };
+
+    assert_eq!(CoordSet::edges(), expected);
+}
+
+#[test]
+fn test_middle_with_omit() {
+    let expected = CoordSet {
+        matrix: Matrix([
+            [false; 5],
+            [false, true, true, true, false],
+            [false, true, true, true, false],
+            [false, true, true, true, false],
+            [false; 5],
+        ]),
+    };
+
+    let mut actual = CoordSet::from(true);
+    actual.omit(&CoordSet::edges());
+    actual.omit(&CoordSet::corners());
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_intersection() {
+    for _ in 0..1000 {
+        let set_a = get_random_set::<30, 30>();
+        let set_b = get_random_set();
+
+        let set_c = set_a.intersection(&set_b);
+
+        for ((a, b), c) in set_a
+            .matrix
+            .iter()
+            .flatten()
+            .zip(set_b.matrix.iter().flatten())
+            .zip(set_c.matrix.iter().flatten())
+        {
+            if *a && *b {
+                assert!(c);
+            } else {
+                assert!(!c);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_omit() {
+    for _ in 0..1000 {
+        let set_a_original = get_random_set::<30, 30>();
+        let mut set_a = set_a_original;
+        let set_b = get_random_set();
+
+        set_a.omit(&set_b);
+
+        for ((a_orig, b), a) in set_a_original
+            .matrix
+            .iter()
+            .flatten()
+            .zip(set_b.matrix.iter().flatten())
+            .zip(set_a.matrix.iter().flatten())
+        {
+            // If statements could be made simpler with boolean algebra, but is
+            // kept like this for clarity
+            if *a_orig {
+                if *b {
+                    assert!(!a);
+                } else {
+                    assert!(a);
+                }
+            } else {
+                assert!(!a);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_extend() {
+    for _ in 0..1000 {
+        let set_a_original = get_random_set::<30, 30>();
+        let mut set_a = set_a_original;
+        let set_b = get_random_set();
+
+        set_a.extend(&set_b);
+
+        for ((a_orig, b), a) in set_a_original
+            .matrix
+            .iter()
+            .flatten()
+            .zip(set_b.matrix.iter().flatten())
+            .zip(set_a.matrix.iter().flatten())
+        {
+            // If statements could be made simpler with boolean algebra, but is
+            // kept like this for clarity
+            if *a_orig || *b {
+                assert!(a);
+            } else {
+                assert!(!a);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_contains() {
+    for _ in 0..100 {
+        let set = get_random_set();
+        let mut clone = CoordSet {
+            matrix: Matrix([[false; 30]; 30]),
+        };
+
+        for (y, row) in clone.matrix.0.iter_mut().enumerate() {
+            for (x, cell) in row.iter_mut().enumerate() {
+                *cell = set.contains(Coord(x as u8, y as u8));
+            }
+        }
+
+        assert_eq!(set, clone);
+    }
+}
+
+#[test]
+fn test_iter() {
+    for _ in 0..100 {
+        let set = get_random_set::<30, 30>();
+        let mut definitely_contained = Vec::new();
+
+        for y in 0..30 {
+            for x in 0..30 {
+                let c = Coord(x as u8, y as u8);
+                if set.contains(c) {
+                    definitely_contained.push(c);
+                }
+            }
+        }
+
+        let iter_collected: Vec<_> = set.iter().collect();
+
+        assert_eq!(iter_collected, definitely_contained);
+    }
+}
+
+#[test]
+fn test_iter_mut() {
+    for _ in 0..100 {
+        let mut set = get_random_set::<5, 5>();
+        let mut other = get_random_set();
+        let clone = set;
+
+        // make sure other is a subset of set, because iter_mut only loops through existant coords
+        let mut inverted = CoordSet::from(true);
+        inverted.omit(&clone);
+        other.omit(&inverted);
+
+        for (exists, coord) in set.iter_mut() {
+            assert!(clone.contains(coord));
+            *exists = other.contains(coord);
+        }
+
+        assert_eq!(set, other);
+    }
+}
+
+fn get_random_set<const W: usize, const H: usize>() -> CoordSet<W, H> {
+    let mut set = CoordSet {
+        matrix: Matrix([[false; W]; H]),
+    };
+    set.matrix.0.fill_with(|| {
+        let mut array = [false; W];
+        array.fill_with(rand::random);
+        array
+    });
+    set
 }
---
+++ weeklyreports/weeklyreport5.md
@@ -0,0 +1,22 @@
+
+# Weekly report 5
+
+1. What have you done this week?
+
+2. How has the project progressed?
+
+3. What did you learn this week / today?
+
+4. What has been unclear or problematic? Please answer this question truthfully, as this is something the course assistant may be able to help with.
+
+5. What next?
+
+## Time spent (46h 50 min)
+
+### Sat 22.04.2023
+
+#### 15:00 - 17:40
+- I did this week's peer review.
+
+#### 19:00 - 00:00
+- Add extensive tests for CoordSet
